16a7eb61a547715fe944ed98d9c9bc197047c98f
Ahmed Olawale A <ahmed4donola1@gmail.com>
2022-10-25 20:04:39 +0000

    [update]: updated models file

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -3,10 +3,15 @@ from django.db.models import Model
 
 # Create your models here.
 class Song(Model):
-    pass
+    first_name = models.CharField(max_length = 150)
+    last_name = models.CharField(max_length = 150)
+    age = models.IntegerField()
 
 class Artiste(Model):
-    pass
+    title = models.CharField(max_length = 150)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Song, on_delete = models.CASCADE)
 
 class Lyrics(Model):
     pass
