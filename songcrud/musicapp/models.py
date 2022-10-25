c9900f8af585f8551142be2249bf5caaf12b8cd0
Ahmed Olawale A <ahmed4donola1@gmail.com>
2022-10-25 19:33:45 +0000

    [update]: updated models file

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
+class Song(models.Model):
+    pass
+
+class Artiste(models.Model):
+    pass
+
+class Lyrics(models.Model):
+    pass
+
