49dda029a1cf4f9dac9ccce51ef0fb7d79962897
Ahmed Olawale A <ahmed4donola1@gmail.com>
2022-10-25 20:13:08 +0000

    [feat]: unlimited charfield

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.db.models import Model
+from unlimited_char.fields import CharField
 
 # Create your models here.
-class Song(Model):
+class Artiste(Model):
     first_name = models.CharField(max_length = 150)
     last_name = models.CharField(max_length = 150)
     age = models.IntegerField()
 
-class Artiste(Model):
+class Song(Model):
     title = models.CharField(max_length = 150)
     date_released = models.DateField()
     likes = models.IntegerField()
-    artiste_id = models.ForeignKey(Song, on_delete = models.CASCADE)
+    artiste_id = models.ForeignKey(Artiste, on_delete = models.CASCADE)
 
 class Lyrics(Model):
-    pass
+    content = CharField()
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
 
