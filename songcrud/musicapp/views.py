efb145fac92c5965d4404a5c92eec52b68d4b3cc
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-11-05 20:29:06 +0000

    [updae]: JSON serializer

--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from musicapp.serializers import SongSerializer, ArtistSerializer
+from musicapp.models import Song, Artiste, Lyrics
+
+class SongViewSet(viewsets.ModelViewSet):
+    queryset = Song.objects.all().order_by('title')
+    serializer_class = SongSerializer
+
+class ArtisteViewSet(viewsets.ModelViewSet):
+    queryset = Artiste.objects.all().order_by('first_name')
+    serializer_class = ArtistSerializer
